8bb339d24b72fea05ebb88d4f8f4f6edbb7d5a2a
kiloexabyte <vothanhdat278+1@gmail.com>
2026-01-29 18:32:33 -0600

    revert build

--- .ops/commands/build.go
+++ .ops/commands/build.go
@@ -3,35 +3,28 @@ package commands
 import (
 	"context"
 	"log"
-	"os"
 
 	"lesiw.io/command"
 	"lesiw.io/command/sys"
 )
 
 func (Ops) Build() {
-	ctx := context.Background()
+	ctx := context.Background() 
 	sh := command.Shell(sys.Machine(), "docker")
 
-	tag := os.Getenv("IMAGE_TAG")
-	if tag == "" {
-		tag = "latest"
-	}
-	imageTag := "kiloexabyte/runner-image:" + tag
-
-	if err := sh.Exec(ctx, "docker",
-		"build",
-		"-t",
-		imageTag, "."); err != nil {
-		log.Fatal(err)
-	}
+	if err := sh.Exec(ctx, "docker", 
+		"build", 
+		"-t", 
+		"kiloexabyte/runner-image", "."); err != nil {
+    	log.Fatal(err)
+    }
 
 	if err := sh.Exec(ctx, "docker",
 		"images",
 		"kiloexabyte/runner-image",
-		"--format",
+		"--format", 
 		"Image Size: {{.Size}}"); err != nil {
 
-		log.Fatal(err)
-	}
+    	log.Fatal(err)
+    }
 }
